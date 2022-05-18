7816acc366d68b5ec1a465708291d7aa62f1d74c
Denis <majestycrew@gmail.com>
2022-05-18 18:33:04 +0300

    Full

--- loginApp/LoginViewController.swift
+++ loginApp/LoginViewController.swift
@@ -14,10 +14,6 @@ class ViewController: UIViewController {
     
     private let login = "Denis"
     private let password = "123"
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let welcomeViewController = segue.destination as? WelcomeViewController else { return }
@@ -28,8 +24,52 @@ class ViewController: UIViewController {
         if userNameTextField.text == login && passwordtextFeild.text == password {
         performSegue(withIdentifier: "segueWelcomeVC", sender: nil)
         } else {
-            return
+            showAlert(with: "Error", and: "The username or password was entered incorrectly.")
         }
+    }
+    
+    @IBAction func helpPressed(_ sender: UIButton) {
+        sender.tag == 0
+        ? showAlert(with: "Oops", and: "Your login is \(login)")
+        : showAlert(with: "Oops", and: "Your password is \(password)")
+    }
+    
+    @IBAction func unwindSegue(segue: UIStoryboardSegue) {
+        userNameTextField.text = ""
+        passwordtextFeild.text = ""
+    }
+}
+
+// MARK: - Alert controller
+
+extension ViewController {
+    
+    private func showAlert(with title: String, and message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let alertOk = UIAlertAction(title: "Ok", style: .default) { _ in
+            self.passwordtextFeild.text = ""
+        }
+        alert.addAction(alertOk)
+        present(alert, animated: true)
+    }
+}
+
+// MARK: - Work with keyboard
+
+extension ViewController: UITextFieldDelegate {
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == userNameTextField {
+            passwordtextFeild.becomeFirstResponder()
+        } else {
+            loginPressed()
+        }
+        return true
     }
     
 }
